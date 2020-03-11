7aba01bdfd5fd3354c9ed2333e7ab117e4fa0c36
DarkforcesII <darkforcesii@gmail.com>
2020-03-11 12:26:28 -0700

    Got Chuck Integration Working

---
+++ Assets/Chunity/Scripts/MyChuckScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MyChuckScript : MonoBehaviour
+{
+    [SerializeField] AudioMixer MyChucK;
+    private string SFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SFX = "SFX";
+        Chuck.Manager.Initialize(MyChucK, SFX);
+        Chuck.Manager.RunCode(SFX, @"SinOsc s => dac;
+0.4 => s.gain;
+
+for (60 => int i; i < 80; i++)
+{
+    Std.mtof(i) => s.freq;
+    40 :: ms => now;
+}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
